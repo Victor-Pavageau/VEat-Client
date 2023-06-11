490685b099752ccee14b3291e4be2313b3d77df8
Victor Pavageau <victor.pavageau@viacesi.fr>
2023-06-11 23:19:38 +0200

    Add authentication page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { ConfigProvider } from "antd";
 import "./App.css";
 import RouteHandler from "./routing";
 
 function App() {
-  return <RouteHandler />;
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#000000',
+        }
+      }}
+    >
+      <RouteHandler />
+    </ConfigProvider>
+  )
 }
 
 export default App;
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,13 +1,18 @@
-import NavBar from "../components/NavBar"
+import { FiChevronLeft } from "react-icons/fi"
 import "./ProfilePage.css"
+import UnauthenticatedBodyProfile from "../components/UnauthenticatedBodyProfile"
+import UnauthenticatedHeaderProfile from "../components/UnauthenticatedHeaderProfile"
 
 function ProfilePage() {
   return (
     <>
-      <NavBar />
       <div className="background-gradient-yellow-orange w-screen h-screen">
-        <div className="flex flex-col px-12 pt-5 justify-center items-center">
-          Profile Page
+        <div className="flex flex-col ml-7 pt-10">
+          <FiChevronLeft size={35} className="justify-start w-fit" onClick={() => { window.history.back() }} />
+        </div>
+        <UnauthenticatedHeaderProfile />
+        <div className="absolute bg-white bottom-0 left-0 right-0 top-56 rounded-t-[2.5rem]">
+          <UnauthenticatedBodyProfile />
         </div>
       </div>
     </>
